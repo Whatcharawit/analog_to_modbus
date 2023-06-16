7f6bdafaffe49ef615362e61a7b587b0188184ac
Artron-Innovative-Co-Ltd <w.whatcharawit@gmail.com>
2023-06-16 16:04:43 +0700

    Write the ananog output

--- src/config.h
+++ src/config.h
@@ -3,10 +3,34 @@
 #include <HardwareSerial.h>
 
 #define mbEnable PB12
-int slaveID = 2;
 
-float humidity;
-float temp;
+#define A1_PIN PA7
+#define A2_PIN PA6
+#define A3_PIN PA5
+#define A4_PIN PA4
+#define A5_PIN PB0
+#define A6_PIN PB1
+
+#define min_out 54
+#define max_out 175
+
+int slaveID = 1;
+
+float Wind_Speed;
+float Wind_Direction;
+float Vertical_Wind_Direction;
+float X_Wind_Speed;
+float Y_Wind_Speed;
+float Z_wind_Speed;
+
+int map_Wind_Speed;
+int map_Wind_Direction;
+int map_Vertivcal_Wind_Direction;
+int map_X_Wind_Speed;
+int map_Y_Wind_Speed;
+int map_Z_Wind_Speed;
+
+int analog_renge = 255;
 
 void preTransmission();
 void postTransmission();
--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,14 @@ void setup()
   mySerial.println("Hello Analog");
 
   pinMode(mbEnable, OUTPUT);
+
+  pinMode(A1_PIN, OUTPUT);
+  pinMode(A2_PIN, OUTPUT);
+  pinMode(A3_PIN, OUTPUT);
+  pinMode(A4_PIN, OUTPUT);
+  pinMode(A5_PIN, OUTPUT);
+  pinMode(A6_PIN, OUTPUT);
+
   node.begin(slaveID, mbSerial);
 
   // Callbacks allow us to configure the RS485 transceiver correctly
@@ -20,23 +28,52 @@ void setup()
 
 void loop() 
 {
-  uint8_t result = node.readHoldingRegisters(32, 2);
+  uint8_t result = node.readHoldingRegisters(0, 6);
 
   if(result == node.ku8MBSuccess)
   {
-    humidity = node.getResponseBuffer(0)/10;
-    temp = node.getResponseBuffer(1)/100;
-    
-    mySerial.print("Humidity : ");
-    mySerial.print(humidity);
-    mySerial.print("\t%RH : ");
-    mySerial.println(temp);
+    Wind_Speed = node.getResponseBuffer(0)/10;
+    Wind_Direction = node.getResponseBuffer(1)/10;
+    Vertical_Wind_Direction = node.getResponseBuffer(2)/10;
+    X_Wind_Speed = node.getResponseBuffer(3)/10;
+    Y_Wind_Speed = node.getResponseBuffer(4)/10;
+    Z_wind_Speed = node.getResponseBuffer(5)/10;
+
+    map_Wind_Speed = map(Wind_Speed, 0.0, 60.0, min_out, max_out);
+    map_Wind_Direction = map(Wind_Direction, 0.0, 360, min_out, max_out);
+    map_Vertivcal_Wind_Direction = map(Vertical_Wind_Direction, -90, 90, min_out, max_out);
+    map_X_Wind_Speed = map(X_Wind_Speed, 0.0, 60.0, min_out, max_out);
+    map_Y_Wind_Speed = map(Y_Wind_Speed, 0.0, 60, min_out, max_out);
+    map_Y_Wind_Speed = map(Z_wind_Speed, 0.0, 60.0, min_out, max_out);
+
+    mySerial.print("Wind Speed              : ");
+    mySerial.println(Wind_Speed);
+    mySerial.print("Wind Direction          : ");
+    mySerial.println(Wind_Direction);
+    mySerial.print("Vertical Wind Direction : ");
+    mySerial.println(Vertical_Wind_Direction);
+    mySerial.print("X Wind Speed            : ");
+    mySerial.println(X_Wind_Speed);
+    mySerial.print("Y Wind Speed            : ");
+    mySerial.println(Y_Wind_Speed);
+    mySerial.print("Z Wind Speed            : ");
+    mySerial.println(Z_wind_Speed);
+    mySerial.println("-------------------------------------");
   }
   else
   {
     mySerial.print("Modbus Error: ");
     mySerial.println(result, HEX);
   }
+
+  // digitalWrite(A1_PIN, LOW);
+  analogWrite(A1_PIN, map_Wind_Speed);
+  analogWrite(A2_PIN, map_Wind_Direction);
+  analogWrite(A3_PIN, map_Vertivcal_Wind_Direction);
+  analogWrite(A4_PIN, map_X_Wind_Speed);
+  analogWrite(A5_PIN, map_Y_Wind_Speed);
+  analogWrite(A6_PIN, map_Z_Wind_Speed);
+
   delay(1000);
 }
 
